08f4b9fc7ca71fc9dafb90069b2f3d103e461c16
lucykirk <c3030925@newcastle.ac.uk>
2025-02-27 13:52:01 +0000

    Car class and constructor set up

---
+++ Car.java
@@ -0,0 +1,13 @@
+public class Car{
+    private String name;
+    private double weight;
+    private double price;
+    private int numberSold;
+    private String type; // hatchback, saloon, estate
+
+    public car(){
+        system.out.println("Creating a car");
+        // Constructor sets up all internal fields of a car
+    }
+
+}
