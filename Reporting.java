b44200848b16055b0d70963b328279a1f8a28d42
lucykirk <c3030925@newcastle.ac.uk>
2025-02-28 12:16:09 +0000

    Reporting class complete

--- Reporting.java
+++ Reporting.java
@@ -1,2 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Reporting {
+    private List<Manufacturer> manufacturers;
+
+    public Reporting(){
+        this.manufacturers = new ArrayList<>();
+    }
+    public void addManufacturer(Manufacturer manufacturer) {
+        this.manufacturers.add(manufacturer);
+    }
+    public List<Manufacturer> getManufacturers(){
+        return this.manufacturers;
+    }
+    public CarModel getMostExpensiveModel() {
+        CarModel mostExpensive = null;
+        for(Manufacturer manufacturer : this.manufacturers){
+        CarModel expensive = manufacturer.getMostExpensiveModel();
+        if(expensive != null && (mostExpensive == null || expensive.getPrice() > mostExpensive.getPrice())){
+            mostExpensive = expensive;
+
+        }
+    }
+        return mostExpensive;
+}
+ public Manufacturer getTopManufacturerByRevenue(String type){
+    Manufacturer topManufacturer = null;
+    double highestRevenue = 0;
+    for(Manufacturer manufacturer : this.manufacturers){
+        double revenue = manufacturer.getRevenueByType(type);
+        if(revenue > highestRevenue){
+            highestRevenue = revenue;
+            topManufacturer = manufacturer;
+        }
+    }
+    return topManufacturer;
+ }
+
+    @Override
+    public String toString() {
+        return "Reporting [manufacturers=" + manufacturers + "]";
+    }
 }
