2bd3ee1ad507b04d122cc4e43ab9ff7cd37440b3
lucykirk <c3030925@newcastle.ac.uk>
2025-02-28 06:15:14 +0000

    Made car manufacturer class

--- Manufacturer.java
+++ Manufacturer.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Manufacturer {
+        private String name;
+        private List<CarModel> carModels;
+        public Manufacturer(String name) {
+            this.name = name;
+            this.carModels = new ArrayList<>();
+        }
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+        public void addCarModel(CarModel carModel) {
+            this.carModels.add(carModel);
+        }
 }
