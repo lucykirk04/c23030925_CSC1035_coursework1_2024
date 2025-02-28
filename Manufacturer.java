58dc48abcc14ee5c0524a90bc9a5596d257f13d4
lucykirk <c3030925@newcastle.ac.uk>
2025-02-28 12:18:37 +0000

    Manufacturer class complete with methods

--- Manufacturer.java
+++ Manufacturer.java
@@ -2,19 +2,62 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Manufacturer {
-        private String name;
-        private List<CarModel> carModels;
-        public Manufacturer(String name) {
-            this.name = name;
-            this.carModels = new ArrayList<>();
+    private String name;
+    private List<CarModel> carModels;
+
+    public Manufacturer(){
+        System.out.println("Manufacturer");
+        this.carModels = new ArrayList<>();
+    }
+
+    public Manufacturer(String name) {
+        this.name = name;
+        this.carModels = new ArrayList<>();
+    }
+
+    public void setName(String name){this.name = name;}
+    public String getName(){ return name; }
+
+    public void addCarModel(CarModel carModel){
+    carModels.add(carModel);
+    }
+    public List<CarModel> getCarModels(){
+    return carModels;}
+
+    public CarModel getMostExpensiveModel() {
+        if (carModels.isEmpty()) return null;
+        CarModel carModel = carModels.get(0);
+        for (CarModel model : carModels) {
+            if (model.getPrice() > expensive.getPrice()) {
+                exensive = model;
+            }
         }
-        public String getName() {
-            return name;
+
+        return expensive;
+
+    }
+
+    public double getRevenueByType(String type) {
+        double totalRevenue = 0;
+        for (CarModel model : carModels) {
+            if (model.getType().equalsIgnoreCase(type)) {
+                totalRevenue += model.getTotalRevenue();
+            }
         }
-        public void setName(String name) {
-            this.name = name;
-        }
-        public void addCarModel(CarModel carModel) {
-            this.carModels.add(carModel);
-        }
+        return totalRevenue;
+
+    }
+
+    public String toString() {
+        return "Manufacturer{name='" + name + "', carModels=" + carModels + "}";
+    }
+
 }
+
+public void main() {
+}
+
+
+
+
+
